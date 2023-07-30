5b8eb3e68b2de1049eb1fc62a649d656e23589bc
SakataAtsuki <hhnkkr@icloud.com>
2023-07-30 23:01:38 +0900

    add gce instance

---
+++ terraform/main/gce.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "default" {
+  name         = "sakata-test"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+  project      = local.project_id
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
